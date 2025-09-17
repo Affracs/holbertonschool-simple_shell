74bf50e402bbb6f6bac82fedc42f896d874cc858
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-09-17 18:57:48 +0400

    Simple shell

--- simple_shell.c
+++ simple_shell.c
@@ -5,77 +5,83 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include "shell.h"
 
-extern char **environ;
 
-#define MAX_COMMAND_LENGTH 1024
-
+/**
+ * main - simple shell
+ *@argc: argument count
+ *@argv: argument vector
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char **argv)
 {
-	char command_path[MAX_COMMAND_LENGTH];
-	pid_t pid;
-	int status;
-	size_t len;
-	unsigned long line_no = 0;
 	int interactive = isatty(STDIN_FILENO);
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
 
 	(void) argc;
+	(void)argv;
 
-	while (1)
+
+while (1)
+{
+	if (interactive)
+		printf("$ ");
+
+	nread = getline(&line, &len, stdin);
+	if (nread == -1)
 	{
-		if (interactive)
-			printf("simple_shell> ");
-
-		if (fgets(command_path, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			if (interactive)
-				printf("\n");
-			break;
-		}
-
-		line_no++;
-		len = strcspn(command_path, "\n");
-		command_path[len] = '\0';
-
-		if (command_path[0] == '\0')
-			continue;
-
-		if (strcmp(command_path, "exit") == 0)
-			break;
-
-		pid = fork();
-		if (pid == -1)
-		{
-			perror(argv[0]);
-			exit(EXIT_FAILURE);
-		}
-		else if (pid == 0)
-		{
-			char *child_argv[] = {command_path, NULL};
-
-			if (execve(command_path, child_argv, environ) == -1)
-			{
-				if (strchr(command_path, '/') != NULL)
-				{
-					fprintf(stderr, "%s: %s: %s\n", argv[0], command_path,
-						(errno == ENOENT) ? "No such file or directory" : "Permission denied");
-				}
-				else
-				{
-					fprintf(stderr, "%s: %lu: %s: not found\n", argv[0], line_no, command_path);
-				}
-				exit(127);
-			}
-		}
-		else
-		{
-			if (waitpid(pid, &status, 0) == -1)
-			{
-				perror(argv[0]);
-				exit(EXIT_FAILURE);
-			}
-		}
+		printf("\n");
+		free(line);
+		exit(0);
 	}
 
-	return (0);
+	if (line[nread - 1] == '\n')
+		line[nread - 1] = '\0';
+
+	if (line[0] == '\0')
+		continue;
+
+	execute_cmd(line, argv[0]);
+}
+free(line);
+return (0);
+}
+
+/**
+ * execute.cmd - executes the command written by the user
+ * @cmd: command to execute
+ * @prog_name: program name
+ */
+void execute_cmd(char *cmd, char *prog_name)
+{
+	pid_t pid;
+	char *argv_exec[2];
+	int status;
+
+	argv_exec[0] = cmd;
+	argv_exec[1] = NULL;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		printf("Error: fork failed\n");
+		return;
+	}
+	if (pid == 0)
+	{
+		if (execve(cmd, argv_exec, environ) == -1)
+		{
+			printf("%s: command does not exist\n", cmd);
+			_exit(1);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
