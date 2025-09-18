51792585809a83ae9e863dcb59851153677919a7
Affracs <alexfrancois1501@gmail.com>
2025-09-18 06:04:12 +0000

    fixed space error with strtok

--- simple_shell.c
+++ simple_shell.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +8,8 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include "main.h"
+
+
 /**
  * main - simple shell
  *@argc: argument count
@@ -20,27 +23,36 @@ int main(int argc, char **argv)
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
+
+
 	(void) argc;
 	(void)argv;
+
+
 while (1)
 {
 	if (interactive)
 		printf("$ ");
+
 	nread = getline(&line, &len, stdin);
 	if (nread == -1)
 	{
 		free(line);
 		exit(0);
 	}
+
 	if (line[nread - 1] == '\n')
 		line[nread - 1] = '\0';
+
 	if (line[0] == '\0')
 		continue;
+
 	execute_cmd(line, argv[0]);
 }
 free(line);
 return (0);
 }
+
 /**
  * execute_cmd - executes the command written by the user
  * @cmd: command to execute
@@ -49,10 +61,19 @@ return (0);
 void execute_cmd(char *cmd, char *prog_name)
 {
 	pid_t pid;
-	char *argv_exec[2];
+	char *argv_exec[64];
 	int status;
-	argv_exec[0] = cmd;
-	argv_exec[1] = NULL;
+	int i = 0;
+	char *token;
+
+	token = strtok(cmd, " ");
+	while (token != NULL && i < 63)
+	{
+		argv_exec[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv_exec[i] = NULL;
+
 	pid = fork();
 	if (pid < 0)
 	{
@@ -61,9 +82,9 @@ void execute_cmd(char *cmd, char *prog_name)
 	}
 	if (pid == 0)
 	{
-		if (execve(cmd, argv_exec, environ) == -1)
+		if (execve(argv_exec[0], argv_exec, environ) == -1)
 		{
-			printf("%s: %s: command does not exist\n", prog_name, cmd);
+			printf("%s: %s: command does not exist\n", prog_name, argv_exec[0]);
 			_exit(1);
 		}
 	}
